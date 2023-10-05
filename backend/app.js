df0fa761f9159a1136301edd5eca1c11c859984f
Tuan Doan <tuandoan14112003@gmail.com>
2023-10-05 23:40:43 +1100

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const digitalAssetRouter = require("./src/routes/DigitalAssetRoutes");
 const authenticationRouter = require("./src/routes/AuthenticationRoutes");
 
 const port = 8000;
 app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/assets", digitalAssetRouter);
