59fd36d722a705be6328bc5fc1421c4ef4605cb6
Tuan Doan <tuandoan14112003@gmail.com>
2023-10-12 18:41:10 +1100

    Add User Service and refactor Authentication Controller

--- backend/src/controllers/AuthenticationController.js
+++ backend/src/controllers/AuthenticationController.js
@@ -1,23 +1,15 @@
-const User = require("./../models/User");
-const jwtUtils = require("./../utils/JwtUtils");
-const web3 = require("../smart-contracts/Web3Instance");
-const DigitalAssetMarketContract = require("../smart-contracts/SmartContract");
+const UserService = require("./../services/UserService");
 const Joi = require("joi");
+const convertJoiValidationError = require("./../utils/ConvertJoiValidationError")
 exports.register = async (req,res) => {
-    const web3Account = web3.eth.accounts.create();
-    const user = new User(req.body.first_name,req.body.last_name,req.body.email,req.body.password,web3Account.privateKey,web3Account.address);
-    let newUser;
-    console.log(web3Account);
+    let userData;
     try {
-        newUser = await User.createUser(user);
+        userData = await UserService.createUser(req.body.first_name,req.body.last_name,req.body.email,req.body.password)
     } catch (error) {
         if (error instanceof Joi.ValidationError) {
-            let errorMessage = error.details
-                .map((err) => err.message.replace(/"/g, ""))
-                .join(", ");
             return res.status(400).json({
                 status: "fail",
-                message: errorMessage,
+                message: convertJoiValidationError(error),
             });
         }
 
@@ -29,77 +21,48 @@ exports.register = async (req,res) => {
         } else {
             return res.status(500).json({
                 status: "error",
-                message: "cannot register a new user"
+                message: error
             })
         }
     }
-
-    const accounts = await web3.eth.getAccounts();
-
-    try {
-        await DigitalAssetMarketContract.methods.createUser(newUser.user_id, newUser.last_name, newUser.email, newUser.wallet_address).send({
-            from: accounts[0],
-            gas: 1000000
-        });
-    } catch (eth_error) {
-        console.log(eth_error);
-        return  res.status(500).json({
-            status: "error",
-            message: eth_error
-        });
-    }
-
     return res.status(200).json({
         status: "success",
         data : {
-            user: newUser,
-            access_token: jwtUtils.generateAccessToken(newUser.user_id,newUser.email)
+            userData
         }
     })
-
 }
 
 
 exports.login = async (req,res) => {
+    let accessToken;
     try {
-        await Joi.object({
-            email: Joi.string().email().required(),
-            password: Joi.string().min(8).required(),
-        }).validateAsync(req.body);
-    } catch (validationError) {
-        let errorMessage = validationError.details.map(err => err.message.replace(/"/g,'')).join(", ");
-        return res.status(400).json({
-            status: "fail",
-            message: errorMessage
-        })
-    }
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-    let users;
-    try {
-        users = await User.findUserByEmail(email)
+        accessToken = await UserService.login(req.body.email,req.body.password);
     } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            message: error
-        })
-    }
+        if (error instanceof Joi.ValidationError) {
+            return res.status(400).json({
+                status: "fail",
+                message: convertJoiValidationError(error),
+            });
+        }
+        else if (error instanceof UserService.InvalidCredentialsError) {
+            return res.status(404).json({
+                status: "error",
+                message: "Invalid credentials"
+            })
+        } else {
+            return res.status(500).json({
+                status: "error",
+                message: error
+            })
+        }
 
-    if (users.length === 0 || users[0].password !== password) {
-        return res.status(404).json({
-            status: "error",
-            message: "Wrong credentials"
-        })
     }
-
-    const user = users[0];
 
     return res.status(200).json({
         status: "success",
         data : {
-            access_token: jwtUtils.generateAccessToken(user.user_id,user.email)
+            access_token: accessToken
         }
     })
 }
---
+++ backend/src/services/UserService.js
@@ -0,0 +1,52 @@
+const web3 = require("../smart-contracts/Web3Instance");
+const User = require("../models/User");
+const Joi = require("joi");
+const DigitalAssetMarketContract = require("../smart-contracts/SmartContract");
+const jwtUtils = require("../utils/JwtUtils");
+
+class InvalidCredentialsError extends Error {
+    constructor() {
+        super("Invalid credentials");
+        this.name = "InvalidCredentialsError";
+    }
+}
+
+
+exports.createUser = async (first_name,last_name,email,password) => {
+    const web3Account = web3.eth.accounts.create();
+    const user = new User(first_name,last_name,email,password,web3Account.privateKey,web3Account.address);
+
+    let newUser = await User.createUser(user);
+    const accounts = await web3.eth.getAccounts();
+    await DigitalAssetMarketContract.methods.createUser(newUser.user_id, newUser.last_name, newUser.email, newUser.wallet_address).send({
+        from: accounts[0],
+        gas: 1000000
+    });
+
+    return {
+        user: newUser,
+        access_token: jwtUtils.generateAccessToken(newUser.user_id,newUser.email)
+    }
+}
+
+exports.login = async (email,password) => {
+    let credentials = {email,password}
+
+    await Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.string().min(8).required(),
+    }).validateAsync(credentials);
+
+    let users = await User.findUserByEmail(email)
+
+    if (users.length === 0 || users[0].password !== password) {
+        throw new InvalidCredentialsError();
+    }
+
+    const user = users[0];
+    return jwtUtils.generateAccessToken(user.user_id, user.email)
+
+}
+
+exports.InvalidCredentialsError = InvalidCredentialsError;
+
